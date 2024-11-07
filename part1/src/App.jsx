e648aa4dce18bb09666cbb5205bf39381ac4bca5
Rpuecar <raqpuecar@gmail.com>
2024-11-07 22:32:55 +0100

    Ejercicios 1.3, 1.4 y 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            <p>
+                {course.parts[0].name} {course.parts[0].exercises}
+            </p>
+            <p>
+                {course.parts[1].name} {course.parts[1].exercises}
+            </p>
+            <p>
+                {course.parts[2].name} {course.parts[2].exercises}
+            </p>
+            <p>
+                Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+            </p>
+        </div>
+    )
 }
 
-export default App
+export default App;
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,8 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
