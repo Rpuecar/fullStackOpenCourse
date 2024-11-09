fd79c07e392c60e81845110e86dd1e385d7eb9a5
Rpuecar <raqpuecar@gmail.com>
2024-11-09 19:04:36 +0100

    Estado del componente, controladores de eventos hasta re-dereizado de la pagina

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -17,6 +17,37 @@ const App = () => {
         ]
     }
 
+// Métodos de objeto y "this"
+    const arto = {
+        name: 'Arto Hellas',
+        greet: function () {
+            console.log('hello, my name is ' + this.name)
+        },
+    }
+
+
+    setTimeout(arto.greet.bind(arto), 1000)
+
+// Clases
+
+    class Person {
+        constructor(name, age) {
+            this.name = name
+            this.age = age
+        }
+
+        greet() {
+            console.log('hello, my name is ' + this.name)
+        }
+    }
+
+    const adam = new Person('Adam Ondra', 29)
+    adam.greet()
+
+    const janja = new Person('Janja Garnbret', 23)
+    janja.greet()
+
+
     return (
         <div>
             <h1>{course.name}</h1>
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import Hello from './Hello'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <App/>
+        <h1>Hello</h1>
+        <Hello name="Maya" age={26}/>
+    </>)
