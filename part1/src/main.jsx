001cdd68cb6173449d4975528811e9ea2aa1eb74
Rpuecar <raqpuecar@gmail.com>
2024-11-10 01:29:58 +0100

    Seguir por controladores de eventos

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,10 +2,15 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 import Hello from './Hello'
+import Counter from "./Counter.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <App/>
         <h1>Hello</h1>
-        <Hello name="Maya" age={26}/>
+        <Hello name="Maya" age={35}/>
+        <Counter/>
     </>)
+
+
+
